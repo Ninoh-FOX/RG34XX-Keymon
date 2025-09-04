a1e7d1e9290db20dfcb4377c7edc18ac8b9c44b0
Ninoh-FOX <ninohfox@gmail.com>
2025-09-04 02:47:07 +0200

    reverse changes

--- src/keymon.c
+++ src/keymon.c
@@ -47,11 +47,10 @@ static int persistent_volume_step = 3;
 static int skip_next_restore = 0;
 
 static const char* ignored_processes[] = {
-    "keymon", "init", "kthreadd", "ksoftirqd", "lru-add-drain",
-    "sleep", "nmcli", "vmstat", "spy", "kbase_event", "nm-dispatcher",
-    "migration", "rcu_", "systemd", "dbus", "getty", "sshd", "sftp-server",
-    "kernel", "worker", "irq/", "mmcqd", "jbd2", "ext4-", "tail",
-    "led_workqueue", "cfg80211", "wpa_supplicant", "dhcpcd", "find",
+    "keymon", "init", "kthreadd", "ksoftirqd", 
+    "migration", "rcu_", "systemd", "dbus", "getty", "sshd",
+    "kernel", "worker", "irq/", "mmcqd", "jbd2", "ext4-", 
+    "led_workqueue", "cfg80211", "wpa_supplicant", "dhcpcd",
     "NetworkManager", "chronyd", "rsyslog", "cron", "bash", "sh"
 };
 
